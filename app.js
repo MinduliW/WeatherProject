4f8c2f59828a32b10a912e2912326cee93f457c1
MinduliW <mwij6229@uni.sydney.edu.au>
2021-04-16 10:13:01 +0530

    all files

---
+++ app.js
@@ -0,0 +1,46 @@
+const express = require("express");
+const https = require("https");
+const bodyParser = require("body-parser");
+const app = express();
+
+
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.get("/", function(req, res){
+
+  res.sendFile(__dirname + "/index.html");
+
+  //res.send("Server is up and running!");
+});
+
+app.post("/", function(req, res){
+    const query = req.body.cityName;
+    const apiKey = "038e27a2bcc0b2a08456050c58700830";
+    const units = "metric";
+    const url = "https://api.openweathermap.org/data/2.5/weather?q=" + query + "&appid=" +apiKey+ "&units=" + units;
+    https.get(url, function(response){
+      //console.log(response.statusCode);
+
+      response.on("data", function(data){
+        const weatherData = JSON.parse(data);
+        const temp = weatherData.main.temp
+        const description = weatherData.weather[0].description
+        const icon = weatherData.weather[0].icon
+        const imageurl = "http://openweathermap.org/img/wn/"+ icon + "@2x.png";
+
+        //console.log(description);
+
+
+        res.write("<h1>The temperature in " + query + " is " + temp + " degrees Celcius.</h1> ");
+        res.write("<h2>The weather is currently " + description + "</h2>")
+        res.write("<img src=" + imageurl + ">")
+        res.send();
+      });
+    });
+
+})
+
+
+app.listen(3000 , function(){
+  console.log("Server running on 3000");
+});
