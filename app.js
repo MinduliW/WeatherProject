f1babfe13d18c1fe40540cb2db4098d06f61bc2b
MinduliW <mwij6229@uni.sydney.edu.au>
2021-06-30 16:01:44 +0530

    port change

--- app.js
+++ app.js
@@ -41,6 +41,8 @@ app.post("/", function(req, res){
 })
 
 
-app.listen(3000 , function(){
-  console.log("Server running on 3000");
-});
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 8000;
+}
+app.listen(port);
